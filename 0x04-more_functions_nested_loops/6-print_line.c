22d86476f9a9d73a3f5d6b21b2aac8daf038aeae
hydeeblack <hydee.sami@gmail.com>
2023-04-27 09:39:49 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,17 +7,17 @@
 void print_line(int n)
 {
 
+int x;
+
 if (n <= 0)
 {
 _putchar('\n');
 }
 else
 {
-int i;
-
-for (i  = 1; i <= n; i++)
+for (x = 0; x < n; x++)
 {
-_putchar('-');
+_putchar(95);
 }
 _putchar('\n');
 }

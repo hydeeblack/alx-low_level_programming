44a3c9df38c5667f8cb003bdf6d255b2feaced2c
hydeeblack <hydee.sami@gmail.com>
2023-05-22 11:54:26 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,33 +21,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name != NULL && owner != NULL)
 	{
 		name_l = _strlen(name) + 1;
-		onw_l = _strlen(owner) + 1;
+		own_l = _strlen(owner) + 1;
 		max_dog = malloc(sizeof(dog_t));
-
+		
 		if (max_dog == NULL)
 			return (NULL);
 		max_dog->name = malloc(sizeof(char) * name_l);
-
+		
 		if (max_dog->name == NULL)
 		{
 			free(max_dog);
 			return (NULL);
 		}
-
+		
 		max_dog->owner = malloc(sizeof(char) * own_l);
-
+		
 		if (max_dog->owner == NULL)
 		{
 			free(max_dog->name);
 			free(max_dog);
 			return (NULL);
 		}
-
+		
 		max_dog->name = _strcpy(max_dog->name, name);
 		max_dog->owner = _strcpy(max_dog->owner, owner);
 		max_dog->age = age;
 	}
-
+	
 	return (max_dog);
 }
 
@@ -61,12 +61,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 int _strlen(char *s)
 {
 	int c = 0;
-
+	
 	for (; *s != '\0'; s++)
 	{
 		c++;
 	}
-
+	
 	return (c);
 }
 
@@ -84,7 +84,7 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[i] =src[i];
+		dest[i] = src[i];
 	}
 
 	dest[i++] = '\0';
